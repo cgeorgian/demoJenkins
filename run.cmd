39dfefd5c300ba8078188b09905cb5a022dbff2c
George Cocis <george.cocis@endava.com>
2019-01-20 23:03:50 +0200

    initial commit

---
+++ run.cmd
@@ -0,0 +1,1 @@
+javadoc -docletpath target\demo-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet com.endava.example.Doclet %*
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javadoc -docletpath target/demo-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet com.endava.example.Doclet "$@"
