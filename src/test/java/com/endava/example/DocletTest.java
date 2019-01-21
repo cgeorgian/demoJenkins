b6c2ed06fd967d574f80a134b82ca4ca8c255624
George Cocis <george.cocis@endava.com>
2019-01-21 13:27:22 +0200

    I think I broke something

--- src/test/java/com/endava/example/DocletTest.java
+++ src/test/java/com/endava/example/DocletTest.java
@@ -3,11 +3,6 @@ package com.endava.example;
 import org.junit.Test;
 
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -17,44 +12,17 @@ import static org.junit.Assert.assertTrue;
 public class DocletTest
 {
     /**
-     * Rigourous Test :-)
+     * The most rigourous test :)
      */
     @Test
-    public void testDoclet() throws URISyntaxException {
-        callJavadoc("com.sample");
+    public void testDocletOK() throws URISyntaxException {
 
         assertTrue(true);
     }
 
-    /**
-     * Calls Javadoc tool.
-     *
-     * <p>The arguments needed to call this project's doclet
-     * and use the test resource's directory as source path
-     * are already included.</p>
-     *
-     * @param extraArgs Any extra arguments to Javadoc
-     */
-    public static void callJavadoc(String ...extraArgs) {
-        final String docletClass = "com.endava.example.Doclet";
+    @Test
+    public void testDocletNotOK() throws URISyntaxException {
 
-        Path root = null;
-        try {
-            root = Paths.get(DocletTest.class.getResource("/root.txt").toURI())
-                    .getParent();
-        } catch (URISyntaxException e) {
-            throw new IllegalStateException("This should never happen", e);
-        }
-
-        List<String> baseArgs = Arrays.asList(
-                "-doclet", docletClass,
-                "-sourcepath", root.toString()
-        );
-        List<String> args = new LinkedList<String>(baseArgs);
-        for (String arg : extraArgs) {
-            args.add(arg);
-        }
-
-        com.sun.tools.javadoc.Main.execute(args.toArray(new String[0]));
+        assertTrue(false);
     }
 }
